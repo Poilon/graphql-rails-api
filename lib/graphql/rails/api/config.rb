27249252eaccee47b1f1ab0257c52419c7a5f992
poilon <poilon@gmail.com>
2018-02-28 09:13:55 +0100

    config

--- lib/graphql/rails/api/config.rb
+++ lib/graphql/rails/api/config.rb
@@ -7,18 +7,18 @@ module Graphql
         attr_accessor :id_type, :basic_mutations
 
         def self.query_resources
-          Dir.glob("#{Rails.root}/app/graphql/*/type.rb").map do |dir|
+          Dir.glob("#{File.expand_path('.')}/app/graphql/*/type.rb").map do |dir|
             dir.split('/').last(2).first
           end
         end
 
         def self.mutation_resources
-          mutations = Dir.glob("#{Rails.root}/app/graphql/*/mutations/*.rb").reject do |e|
+          mutations = Dir.glob("#{File.expand_path('.')}/app/graphql/*/mutations/*.rb").reject do |e|
             e.end_with?('type.rb', 'types.rb')
           end
           mutations = mutations.map { |e| e.split('/').last.gsub('.rb', '') }.uniq
           mutations.each_with_object({}) do |meth, h|
-            h[meth] = Dir.glob("#{Rails.root}/app/graphql/*/mutations/#{meth}.rb").map do |dir|
+            h[meth] = Dir.glob("#{File.expand_path('.')}/app/graphql/*/mutations/#{meth}.rb").map do |dir|
               dir.split('/').last(3).first
             end
           end
