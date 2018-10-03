0497c776d58168b594a59a6efcc24874ea204c47
Poilon <poilon@gmail.com>
2018-10-03 10:58:49 +0200

    connection

---
+++ lib/generators/graphql_connections/graphql_all_connections_generator.rb
@@ -0,0 +1,26 @@
+class GraphqlAllConnectionsGenerator < Rails::Generators::NamedBase
+
+  def generate
+    Graphql::Rails::Api::Config.query_resources.each do |resource|
+      dir = "app/graphql/#{resource.pluralize}"
+      generate_connection(dir, resource)
+    end
+  end
+
+  private
+
+  def generate_connection(dir, resource)
+    File.write(
+      "#{dir}/connection.rb",
+      <<~STRING
+        #{resource.pluralize.camelize}::Connection = #{resource.pluralize.camelize}::Type.define_connection do
+          name '#{resource.camelize}Connection'
+          field :total_count, types.Int do
+            resolve ->(obj, _, _) { obj.nodes.count }
+          end
+        end
+      STRING
+    )
+  end
+
+end
