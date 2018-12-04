1b7ec022c5ee7bad4fd65fed43fac522849bf716
Poilon <poilon@gmail.com>
2018-12-04 18:14:13 +0100

    remove graphql subscriptions

--- lib/generators/graphql_rails_api/install_generator.rb
+++ lib/generators/graphql_rails_api/install_generator.rb
@@ -22,7 +22,6 @@ module GraphqlRailsApi
       if options.action_cable_subs?
         write_websocket_connection
         write_online_users_channel
-        write_graphql_channel
       end
       write_initializer
       write_require_application_rb
@@ -138,9 +137,6 @@ module GraphqlRailsApi
           config = Graphql::Rails::Api::Config.instance
 
           config.id_type = #{options.pg_uuid? ? ':uuid' : ':id'} # :id or :uuid
-
-          # Possibilites are :create, :update or :destroy
-          config.basic_mutations = %i[create update destroy]
         STRING
       )
     end
@@ -204,62 +200,6 @@ module GraphqlRailsApi
 
           end
 
-        STRING
-      )
-    end
-
-    def write_graphql_channel
-      File.write(
-        'app/channels/graphql_channel.rb',
-        <<~STRING
-          class GraphqlChannel < ApplicationCable::Channel
-
-            def subscribed
-              @subscription_ids = []
-            end
-
-            # see graphql-ruby from details
-            def execute(data)
-              query, context, variables, operation_name = options_for_execute(data)
-              result = #{@app_name.camelize}Schema.execute(query: query, context: context,
-                                              variables: variables, operation_name: operation_name)
-              payload = { result: result.subscription? ? nil : result.to_h, more: result.subscription?,
-                          errors: result ? result.to_h[:errors] : nil }
-              @subscription_ids << result.context[:subscription_id] if result.context[:subscription_id]
-              transmit(payload)
-            end
-
-            def unsubscribed
-              @subscription_ids.each do |sid|
-                #{@app_name.camelize}Schema.subscriptions.delete_subscription(sid)
-              end
-            end
-
-            def options_for_execute(data)
-              query = data['query']
-              variables = ensure_hash(data['variables'])
-              operation_name = data['operationName']
-              context = { current_user: current_user, channel: self }.
-                        merge(ensure_hash(data['context']).symbolize_keys). # ensure context is filled
-                        merge(variables) # include variables in context too
-              [query, context, variables, operation_name]
-            end
-
-            # Handle form data, JSON body, or a blank value
-            def ensure_hash(ambiguous_param)
-              case ambiguous_param
-              when String
-                ambiguous_param.present? ? ensure_hash(JSON.parse(ambiguous_param)) : {}
-              when Hash, ActionController::Parameters
-                ambiguous_param
-              when nil
-                {}
-              else
-                raise ArgumentError, 'Unexpected parameter: ' + ambiguous_param
-              end
-            end
-
-          end
         STRING
       )
     end
