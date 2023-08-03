d5f2607488437ac012ae94b95e50a3051623f9c2
Poilon <poilon@gmail.com>
2023-08-03 10:38:34 +0200

    mutations

--- lib/generators/graphql_rails_api/install_generator.rb
+++ lib/generators/graphql_rails_api/install_generator.rb
@@ -80,7 +80,6 @@ module GraphqlRailsApi
     all
   end
 
-
         STRING
       )
     end
@@ -109,12 +108,167 @@ module GraphqlRailsApi
     def write_initializer
       File.write(
         "config/initializers/graphql_rails_api_config.rb",
-        <<~STRING
-          require 'graphql/rails/api/config'
+        <<-STRING
+          require "graphql/rails/api/config"
 
           config = Graphql::Rails::Api::Config.instance
         STRING
       )
+    end
+
+    def write_service
+      File.write(
+        'app/graphql/application_service.rb',
+        <<~'STRING'
+          class ApplicationService
+
+            attr_accessor :params, :object, :fields, :user
+
+            def initialize(params: {}, object: nil, object_id: nil, user: nil, context: nil)
+              @params = params.is_a?(Array) ? params.map { |p| p.to_h.symbolize_keys } : params.to_h.symbolize_keys
+              @context = context
+              @object = object || (object_id && model.visible_for(user: user).find_by(id: object_id))
+              @object_id = object_id
+              @user = user
+            end
+
+            def self.call(resource, meth)
+              lambda { |_obj, args, context|
+                params = args && args[resource] ? args[resource] : args
+                "#{resource.to_s.pluralize.camelize.constantize}::Service".constantize.new(
+                  params: params, user: context[:current_user],
+                  object_id: args[:id], context: context
+                ).send(meth)
+              }
+            end
+
+            def index
+              Graphql::HydrateQuery.new(
+                model.all,
+                @context,
+                order_by: params[:order_by],
+                filter: params[:filter],
+                per_page: params[:per_page],
+                page: params[:page],
+                user: user
+              ).run.compact
+            end
+
+            def show
+              object = Graphql::HydrateQuery.new(model.all, @context, user: user, id: params[:id]).run
+              return not_allowed if object.blank?
+
+              object
+            end
+
+            def create
+              object = model.new(params.select { |p| model.new.respond_to?(p) })
+              return not_allowed if not_allowed_to_create_resource(object)
+
+              if object.save
+                { singular_resource => Graphql::HydrateQuery.new(model.all, @context, user: user, id: object.id).run }
+              else
+                graphql_error(object.errors.full_messages.join(', '))
+              end
+            end
+
+            def update
+              return not_allowed if write_not_allowed
+
+              if object.update_attributes(params)
+                { singular_resource => Graphql::HydrateQuery.new(model.all, @context, user: user, id: object.id).run }
+              else
+                graphql_error(object.errors.full_messages.join(', '))
+              end
+            end
+
+            def destroy
+              object = model.find_by(id: params[:id])
+              return not_allowed if write_not_allowed
+
+              if object.destroy
+                { singular_resource => object.attributes }
+              else
+                graphql_error(object.errors.full_messages.join(', '))
+              end
+            end
+
+            private
+
+            def write_not_allowed
+              !model.visible_for(user: user).include?(object) if object
+            end
+
+            def access_not_allowed
+              !model.visible_for(user: user).include?(object) if object
+            end
+
+            def not_allowed
+              graphql_error('403 - Not allowed')
+            end
+
+            def graphql_error(message)
+              GraphQL::ExecutionError.new(message)
+            end
+
+            def singular_resource
+              resource_name.singularize
+            end
+
+            def model
+              singular_resource.camelize.constantize
+            end
+
+            def resource_name
+              self.class.to_s.split(':').first.underscore
+            end
+
+            def not_allowed_to_create_resource(created_resource)
+              params.select { |k, _| k.to_s.end_with?('_id') }.each do |belongs_relation, rel_id|
+                klass = created_resource.class.reflect_on_association(belongs_relation.to_s.gsub('_id', '')).klass
+                return true if rel_id.present? && !klass.visible_for(user: user).pluck(:id).include?(rel_id)
+              end
+
+              params.select { |k, _| k.to_s.end_with?('_ids') }.each do |many_relation, rel_ids|
+                klass = created_resource.class.reflect_on_association(many_relation.to_s.gsub('_ids', '').pluralize).klass
+                ids = klass.visible_for(user: user).pluck(:id)
+                rel_ids.each { |id| return true if id.present? && !ids.include?(id) }
+              end
+              false
+            end
+
+          end
+        STRING
+      )
+    end    
+
+    def write_schema
+      logger = <<~'STRING'
+        type_error_logger = Logger.new("#{Rails.root}/log/graphql_type_errors.log")
+      STRING
+
+      error_handler = <<~'STRING'
+        type_error_logger.error(
+              "#{err} for #{query_ctx.query.query_string} with #{query_ctx.query.provided_variables}"
+            )
+      STRING
+
+      File.write(
+        "app/graphql/#{@app_name}_schema.rb",
+        <<~STRING
+            class #{@app_name.camelize}Schema < GraphQL::Schema
+
+              query QueryType
+              mutation MutationType
+              max_depth 15
+
+              def self.type_error(err, query_ctx)
+                #{logger}
+                #{error_handler}  end
+
+            end
+          STRING
+        )
     end
 
     def write_controller
@@ -249,162 +403,7 @@ module GraphqlRailsApi
       )
     end
 
-    def write_schema
-      logger = <<~'STRING'
-        type_error_logger = Logger.new("#{Rails.root}/log/graphql_type_errors.log")
-      STRING
 
-      error_handler = <<~'STRING'
-        type_error_logger.error "#{err} for #{query_ctx.query.query_string} \
-            with #{query_ctx.query.provided_variables}"
-      STRING
-
-      File.write(
-        "app/graphql/#{@app_name}_schema.rb",
-        <<~STRING
-                class #{@app_name.camelize}Schema < GraphQL::Schema
-                  query QueryType
-                  mutation MutationType
-                  max_depth 15
-
-                  def self.type_error(err, query_ctx)
-                    type_error_logger = Logger.new("#{Rails.root}/log/graphql_type_errors.log")
-
-                    type_error_logger.error(
-                      "#{err} for #{query_ctx.query.query_string} with #{query_ctx.query.provided_variables}"
-                    )
-                  end
-                end
-              <<~STRING
-            )
-          end
-
-          def write_service
-            File.write(
-              'app/graphql/application_service.rb',
-              <<~'STRING'
-                class ApplicationService
-
-                  attr_accessor :params, :object, :fields, :user
-
-                  def initialize(params: {}, object: nil, object_id: nil, user: nil, context: nil)
-                    @params = params.is_a?(Array) ? params.map { |p| p.to_h.symbolize_keys } : params.to_h.symbolize_keys
-                    @context = context
-                    @object = object || (object_id && model.visible_for(user: user).find_by(id: object_id))
-                    @object_id = object_id
-                    @user = user
-                  end
-
-                  def self.call(resource, meth)
-                    lambda { |_obj, args, context|
-                      params = args && args[resource] ? args[resource] : args
-                      "#{resource.to_s.pluralize.camelize.constantize}::Service".constantize.new(
-                        params: params, user: context[:current_user],
-                        object_id: args[:id], context: context
-                      ).send(meth)
-                    }
-                  end
-
-                  def index
-                    Graphql::HydrateQuery.new(
-                      model.all,
-                      @context,
-                      order_by: params[:order_by],
-                      filter: params[:filter],
-                      per_page: params[:per_page],
-                      page: params[:page],
-                      user: user
-                    ).run.compact
-                  end
-
-                  def show
-                    object = Graphql::HydrateQuery.new(model.all, @context, user: user, id: params[:id]).run
-                    return not_allowed if object.blank?
-
-                    object
-                  end
-
-                  def create
-                    object = model.new(params.select { |p| model.new.respond_to?(p) })
-                    return not_allowed if not_allowed_to_create_resource(object)
-
-                    if object.save
-                      { singular_resource => Graphql::HydrateQuery.new(model.all, @context, user: user, id: object.id).run }
-                    else
-                      graphql_error(object.errors.full_messages.join(', '))
-                    end
-                  end
-
-                  def update
-                    return not_allowed if write_not_allowed
-
-                    if object.update_attributes(params)
-                      { singular_resource => Graphql::HydrateQuery.new(model.all, @context, user: user, id: object.id).run }
-                    else
-                      graphql_error(object.errors.full_messages.join(', '))
-                    end
-                  end
-
-                  def destroy
-                    object = model.find_by(id: params[:id])
-                    return not_allowed if write_not_allowed
-
-                    if object.destroy
-                      { singular_resource => object.attributes }
-                    else
-                      graphql_error(object.errors.full_messages.join(', '))
-                    end
-                  end
-
-                  private
-
-                  def write_not_allowed
-                    !model.visible_for(user: user).include?(object) if object
-                  end
-
-                  def access_not_allowed
-                    !model.visible_for(user: user).include?(object) if object
-                  end
-
-                  def not_allowed
-                    graphql_error('403 - Not allowed')
-                  end
-
-                  def graphql_error(message)
-                    GraphQL::ExecutionError.new(message)
-                  end
-
-                  def singular_resource
-                    resource_name.singularize
-                  end
-
-                  def model
-                    singular_resource.camelize.constantize
-                  end
-
-                  def resource_name
-                    self.class.to_s.split(':').first.underscore
-                  end
-
-                  def not_allowed_to_create_resource(created_resource)
-                    params.select { |k, _| k.to_s.end_with?('_id') }.each do |belongs_relation, rel_id|
-                      klass = created_resource.class.reflect_on_association(belongs_relation.to_s.gsub('_id', '')).klass
-                      return true if rel_id.present? && !klass.visible_for(user: user).pluck(:id).include?(rel_id)
-                    end
-
-                    params.select { |k, _| k.to_s.end_with?('_ids') }.each do |many_relation, rel_ids|
-                      klass = created_resource.class.reflect_on_association(many_relation.to_s.gsub('_ids', '').pluralize).klass
-                      ids = klass.visible_for(user: user).pluck(:id)
-                      rel_ids.each { |id| return true if id.present? && !ids.include?(id) }
-                    end
-                    false
-                  end
-
-                end
-
-        STRING
-      )
-    end
 
     def write_at(file_name, line, data)
       open(file_name, "r+") do |f|
